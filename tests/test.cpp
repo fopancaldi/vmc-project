40f5654caf0fea736ccfac1602dba9f0607b3c28
Francesco Orso Pancaldi <francesco.orso.pancaldi@gmail.com>
2024-10-31 09:27:32 +0000

    Add some tests for 1b.

---
+++ tests/test.cpp
@@ -0,0 +1,141 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#include "vmcp.hpp"
+
+#include <numbers>
+#include <tuple>
+
+constexpr vmcp::UIntType seed = 64826;
+// TODO: Rename this
+constexpr vmcp::FPType varianceTolerance = 1e-9f;
+
+// TODO: Rename if WrappedVMCEnergies is renamed
+TEST_CASE("Testing WrappedVMCEnergies_") {
+    SUBCASE("1D harmonic oscillator") {
+        struct WavefHO {
+            vmcp::FPType m;
+            vmcp::FPType omega;
+            vmcp::FPType operator()(vmcp::Positions<1, 1> x, vmcp::VarParams<0>) const {
+                return std::pow(std::numbers::e_v<vmcp::FPType>,
+                                -x[0][0].val * x[0][0].val * (m * omega / (2 * vmcp::hbar)));
+            }
+        };
+        struct PotHO {
+            vmcp::FPType m;
+            vmcp::FPType omega;
+            vmcp::FPType operator()(vmcp::Positions<1, 1> x) const {
+                return x[0][0].val * x[0][0].val * (m * omega * omega / 2);
+            }
+        };
+        struct KinHO {
+            vmcp::FPType m;
+            vmcp::FPType omega;
+            vmcp::FPType operator()(vmcp::Positions<1, 1> x, vmcp::VarParams<0>) const {
+                // TODO: Constructing a new object might be expensive
+                return (vmcp::hbar * omega / 2) * (1 - x[0][0].val * x[0][0].val * (m * omega / vmcp::hbar)) *
+                       WavefHO{m, omega}(x, vmcp::VarParams<0>{});
+            }
+        };
+
+        vmcp::IntType const numberEnergies = 100;
+        vmcp::Bounds<1> const bounds = {vmcp::Bound{-100, 100}};
+        vmcp::RandomGenerator rndGen{seed};
+        vmcp::FPType const mInit = 0.1f;
+        vmcp::FPType const omegaInit = 0.1f;
+        vmcp::FPType const mStep = 0.2f;
+        vmcp::FPType const omegaStep = 0.2f;
+        vmcp::IntType const mIterations = 40;
+        vmcp::IntType const omegaIterations = 40;
+        WavefHO wavefHO{mInit, omegaInit};
+        PotHO potHO{mInit, omegaInit};
+        KinHO kinHO{mInit, omegaInit};
+
+        for (auto [i, m_] = std::tuple{vmcp::IntType{0}, mInit}; i != mIterations; ++i, m_ += mStep) {
+            wavefHO.m = m_;
+            potHO.m = m_;
+            kinHO.m = m_;
+            for (auto [j, omega_] = std::tuple{vmcp::IntType{0}, omegaInit}; j != omegaIterations;
+                 ++j, omega_ += omegaStep) {
+                wavefHO.omega = omega_;
+                potHO.omega = omega_;
+                kinHO.omega = omega_;
+
+                vmcp::Energy expectedEn{vmcp::hbar * omega_ / 2};
+                vmcp::VMCResult vmcr = vmcp::VMCEnergy<1, 1>(wavefHO, vmcp::VarParams<0>{}, kinHO, potHO,
+                                                             bounds, numberEnergies, rndGen);
+
+                // If due to numerical errors the variance is very small, use varianceTolerance instead
+                // If the variance is negative and larger in absolute value than the tolerance, the check will
+                // fail
+                if (std::abs(vmcr.variance.val) < varianceTolerance) {
+                    CHECK(std::abs(vmcr.energy.val - expectedEn.val) < varianceTolerance);
+                } else {
+                    CHECK(std::abs(vmcr.energy.val - expectedEn.val) < vmcr.variance.val);
+                }
+            }
+        }
+    }
+
+    SUBCASE("1D potential box") {
+        // l = length of the box
+        struct WavefBox {
+            vmcp::FPType m;
+            vmcp::FPType l;
+            vmcp::FPType operator()(vmcp::Positions<1, 1> x, vmcp::VarParams<0>) const {
+                return std::cos(std::numbers::pi_v<vmcp::FPType> * x[0][0].val / l);
+            }
+        };
+        struct KinBox {
+            vmcp::FPType m;
+            vmcp::FPType l;
+            vmcp::FPType operator()(vmcp::Positions<1, 1> x, vmcp::VarParams<0>) const {
+                // TODO: Constructing a new object might be expensive
+                return 1 / (2 * m) * std::pow(vmcp::hbar * std::numbers::pi_v<vmcp::FPType> / l, 2) *
+                       WavefBox{m, l}(x, vmcp::VarParams<0>{});
+            }
+        };
+
+        vmcp::IntType const numberEnergies = 100;
+        vmcp::RandomGenerator rndGen{seed};
+        vmcp::FPType const mInit = 0.1f;
+        vmcp::FPType const lInit = 0.5f;
+        vmcp::FPType const mStep = 0.2f;
+        vmcp::FPType const lStep = 0.2f;
+        vmcp::IntType const mIterations = 40;
+        vmcp::IntType const lIterations = 40;
+        WavefBox wavefBox{mInit, lInit};
+        KinBox kinBox{mInit, lInit};
+
+        for (auto [i, m_] = std::tuple{vmcp::IntType{0}, mInit}; i != mIterations; ++i, m_ += mStep) {
+            wavefBox.m = m_;
+            kinBox.m = m_;
+            for (auto [j, l_] = std::tuple{vmcp::IntType{0}, lInit}; j != lIterations; ++j, l_ += lStep) {
+                wavefBox.l = l_;
+                kinBox.l = l_;
+
+                vmcp::Energy expectedEn{1 / (2 * m_) *
+                                        std::pow(vmcp::hbar * std::numbers::pi_v<vmcp::FPType> / l_, 2)};
+                vmcp::VMCResult vmcr = vmcp::VMCEnergy<1, 1>(
+                    wavefBox, vmcp::VarParams<0>{}, kinBox,
+                    [](vmcp::Positions<1, 1>) { return vmcp::FPType{0}; },
+                    vmcp::Bounds<1>{vmcp::Bound{-l_ / 2, l_ / 2}}, numberEnergies, rndGen);
+
+                // If due to numerical errors the variance is very small, use varianceTolerance instead
+                // If the variance is negative and larger in absolute value than the tolerance, the check will
+                // fail
+                if (std::abs(vmcr.variance.val) < varianceTolerance) {
+                    CHECK(std::abs(vmcr.energy.val - expectedEn.val) < varianceTolerance);
+                } else {
+                    CHECK(std::abs(vmcr.energy.val - expectedEn.val) < vmcr.variance.val);
+                }
+            }
+        }
+    }
+
+    // TODO:
+    SUBCASE("1D triangular well potential") {}
+
+    // TODO:
+    SUBCASE("1D radial Schroedinger eq") {}
+}
