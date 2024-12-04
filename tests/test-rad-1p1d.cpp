35f57c70fdf32433a1ad79fcb514edd237cc6e94
Lorenzo Fabbri <lorenzo.fabbri26@studio.unibo.it>
2024-12-04 11:10:03 +0100

    Update and rename test-rad-1p3d.cpp to test-rad-1p1d.cpp
    Changed in one dimension and fixed wavefunction, some problems are still present (problematic tests are commented)

--- tests/test-rad-1p1d.cpp
+++ tests/test-rad-1p1d.cpp
@@ -1,5 +1,5 @@
 //!
-//! @file test-rad-1p3d.cpp
+//! @file test-rad-1p1d.cpp
 //! @brief Tests for the hydrogen atom radial problem, with one particle in cartesian coordinates (i.e.
 //! 3 dimensions)
 //! @authors Lorenzo Fabbri, Francesco Orso Pancaldi
@@ -20,10 +20,8 @@ TEST_CASE("Testing the radial problem") {
     std::ofstream file_stream;
     file_stream.open(logFilePath, std::ios_base::app);
 
-    SUBCASE("3D radial Schrodinger eq") {
-        vmcp::CoordBounds<3> const coordBound = {vmcp::Bound{vmcp::Coordinate{-100}, vmcp::Coordinate{100}},
-                                                 vmcp::Bound{vmcp::Coordinate{-100}, vmcp::Coordinate{100}},
-                                                 vmcp::Bound{vmcp::Coordinate{-100}, vmcp::Coordinate{100}}};
+    SUBCASE("1D radial Schrodinger eq") {
+        vmcp::CoordBounds<1> const coordBound = {vmcp::Bound{vmcp::Coordinate{-100}, vmcp::Coordinate{100}}};
         vmcp::RandomGenerator rndGen{seed};
         vmcp::Mass const mInit{1.f};
         vmcp::FPType const kInit{1.f};
@@ -31,69 +29,43 @@ TEST_CASE("Testing the radial problem") {
         vmcp::FPType const kStep{0.1f};
         vmcp::IntType const mIterations = iterations;
         vmcp::IntType const kIterations = iterations;
-        vmcp::FPType constexpr waveCutoff{0.001f};
 
         struct PotRad {
             vmcp::FPType k;
-            vmcp::FPType operator()(vmcp::Positions<3, 1> x) const {
-                if ((std::abs(x[0][0].val) < waveCutoff) && (std::abs(x[0][1].val) < waveCutoff) &&
-                    (std::abs(x[0][2].val) < waveCutoff)) {
-                    return vmcp::FPType{0.f};
-                } else {
-                    return -k / (std::sqrt(x[0][0].val * x[0][0].val + x[0][1].val * x[0][1].val +
-                                           x[0][2].val * x[0][2].val));
-                }
-            }
+            vmcp::FPType operator()(vmcp::Positions<1, 1> r) const { return -k / std::abs(r[0][0].val); }
         };
 
         SUBCASE("No variational parameters, with Metropolis or importance sampling") {
             struct WavefRad {
                 vmcp::Mass m;
                 vmcp::FPType k;
-                vmcp::FPType operator()(vmcp::Positions<3, 1> x, vmcp::VarParams<0>) const {
+                vmcp::FPType operator()(vmcp::Positions<1, 1> r, vmcp::VarParams<0>) const {
                     vmcp::FPType a0 = vmcp::hbar * vmcp::hbar / (k * m.val);
-                    if ((std::abs(x[0][0].val) < waveCutoff) && (std::abs(x[0][1].val) < waveCutoff) &&
-                        (std::abs(x[0][2].val) < waveCutoff)) {
-                        return vmcp::FPType{0.f};
-                    } else {
-                        return std::exp(-std::sqrt(x[0][0].val * x[0][0].val + x[0][1].val * x[0][1].val +
-                                                   x[0][2].val * x[0][2].val) /
-                                        a0);
-                    }
+                    return std::abs(r[0][0].val) * std::exp(-std::abs(r[0][0].val) / a0);
                 }
             };
             struct FirstDerRad {
                 vmcp::Mass m;
                 vmcp::FPType k;
-                vmcp::IntType dimension;
-                FirstDerRad(vmcp::Mass m_, vmcp::FPType k_, vmcp::IntType dimension_)
-                    : m{m_}, k{k_}, dimension{dimension_} {
-                    assert(dimension >= 0);
-                    assert(dimension <= 2);
-                }
-                vmcp::FPType operator()(vmcp::Positions<3, 1> x, vmcp::VarParams<0>) const {
+                vmcp::FPType operator()(vmcp::Positions<1, 1> r, vmcp::VarParams<0>) const {
                     vmcp::FPType a0 = vmcp::hbar * vmcp::hbar / (k * m.val);
-                    vmcp::UIntType uDim = static_cast<vmcp::UIntType>(dimension);
-                    return -x[0][uDim].val * WavefRad{m, k}(x, vmcp::VarParams<0>{}) /
-                           (a0 * std::sqrt(x[0][0].val * x[0][0].val + x[0][1].val * x[0][1].val +
-                                           x[0][2].val * x[0][2].val));
+                    return (std::exp(-std::abs(r[0][0].val) / a0) -
+                            std::abs(r[0][0].val) * std::exp(std::abs(r[0][0].val) / a0)) *
+                           std::copysign(1.f, r[0][0].val);
                 }
             };
             struct LaplRad {
                 vmcp::Mass m;
                 vmcp::FPType k;
-                vmcp::FPType operator()(vmcp::Positions<3, 1> x, vmcp::VarParams<0>) const {
+                vmcp::FPType operator()(vmcp::Positions<1, 1> r, vmcp::VarParams<0>) const {
                     vmcp::FPType a0 = vmcp::hbar * vmcp::hbar / (k * m.val);
-
-                    return (WavefRad{m, k}(x, vmcp::VarParams<0>{}) / a0) *
-                           ((1 / a0) - 2 / std::sqrt(x[0][0].val * x[0][0].val + x[0][1].val * x[0][1].val +
-                                                     x[0][2].val * x[0][2].val));
+                    return (WavefRad{m, k}(r, vmcp::VarParams<0>{}) / a0) *
+                           (1 / a0 - 2 / std::abs(r[0][0].val));
                 }
             };
 
             WavefRad wavefRad{mInit, kInit};
-            vmcp::Gradients<3, 1, FirstDerRad> gradRad{
-                FirstDerRad{mInit, kInit, 0}, FirstDerRad{mInit, kInit, 1}, FirstDerRad{mInit, kInit, 2}};
+            vmcp::Gradients<1, 1, FirstDerRad> gradRad{FirstDerRad{mInit, kInit}};
             vmcp::Laplacians<1, LaplRad> laplRad{LaplRad{mInit, kInit}};
             PotRad potRad{kInit};
 
@@ -103,23 +75,19 @@ TEST_CASE("Testing the radial problem") {
                 potRad.k = k_;
                 wavefRad.k = k_;
                 gradRad[0][0].k = k_;
-                gradRad[0][1].k = k_;
-                gradRad[0][2].k = k_;
                 laplRad[0].k = k_;
 
                 for (auto [i, m_] = std::tuple{vmcp::IntType{0}, mInit}; i != mIterations; ++i, m_ += mStep) {
                     wavefRad.m = m_;
                     gradRad[0][0].m = m_;
-                    gradRad[0][1].m = m_;
-                    gradRad[0][2].m = m_;
                     laplRad[0].m = m_;
 
                     vmcp::Energy const expectedEn{-k_ * k_ * m_.val / (2 * vmcp::hbar * vmcp::hbar)};
                     vmcp::VMCResult const vmcrMetr =
-                        vmcp::VMCEnergy<3, 1, 0>(wavefRad, vmcp::ParamBounds<0>{}, laplRad, std::array{m_},
+                        vmcp::VMCEnergy<1, 1, 0>(wavefRad, vmcp::ParamBounds<0>{}, laplRad, std::array{m_},
                                                  potRad, coordBound, numEnergies, rndGen);
                     vmcp::VMCResult const vmcrImpSamp =
-                        vmcp::VMCEnergy<3, 1, 0>(wavefRad, vmcp::ParamBounds<0>{}, gradRad, laplRad,
+                        vmcp::VMCEnergy<1, 1, 0>(wavefRad, vmcp::ParamBounds<0>{}, gradRad, laplRad,
                                                  std::array{m_}, potRad, coordBound, numEnergies, rndGen);
 
                     std::string logMessage{"mass: " + std::to_string(m_.val) +
@@ -141,47 +109,35 @@ TEST_CASE("Testing the radial problem") {
             }
         }
 
-        /*
+        /* LF FIXME: 4 out of 8 variational test fail
                 SUBCASE("One variational parameter") {
-                    PotRad potRad{kInit};
-                    auto const wavefRad{[](vmcp::Positions<3, 1> x, vmcp::VarParams<1> alpha) {
-                        if ((std::abs(x[0][0].val) < waveCutoff) && (std::abs(x[0][1].val) < waveCutoff) &&
-                            (std::abs(x[0][2].val) < waveCutoff)) {
-                            return vmcp::FPType{0.f};
-                        } else {
-                            return std::exp(-std::sqrt(x[0][0].val * x[0][0].val + x[0][1].val * x[0][1].val +
-                                                       x[0][2].val * x[0][2].val) *
-                                            alpha[0].val);
-                        }
+                    auto const wavefRad{[](vmcp::Positions<1, 1> r, vmcp::VarParams<1> alpha) {
+                        return std::abs(r[0][0].val) * std::exp(-std::abs(r[0][0].val) * alpha[0].val);
                     }};
 
-                    std::array laplRad{[](vmcp::Positions<3, 1> x, vmcp::VarParams<1> alpha) {
-                        if ((std::abs(x[0][0].val) < waveCutoff) && (std::abs(x[0][1].val) < waveCutoff) &&
-                            (std::abs(x[0][2].val) < waveCutoff)) {
-                            return vmcp::FPType{0.f};
-                        } else {
-                            return (std::exp(-std::sqrt(x[0][0].val * x[0][0].val + x[0][1].val * x[0][1].val
-           + x[0][2].val * x[0][2].val) * alpha[0].val) * alpha[0].val) * (alpha[0].val - 2 /
-           std::sqrt(x[0][0].val * x[0][0].val + x[0][1].val * x[0][1].val + x[0][2].val * x[0][2].val));
-                        }
+                    std::array laplRad{[](vmcp::Positions<1, 1> r, vmcp::VarParams<1> alpha) {
+                        return std::abs(r[0][0].val) * std::exp(-std::abs(r[0][0].val) * alpha[0].val) *
+                               alpha[0].val * (alpha[0].val - (2 / std::abs(r[0][0].val)));
                     }};
 
                     auto start = std::chrono::high_resolution_clock::now();
-                    for (auto [i, m_] = std::tuple{vmcp::IntType{0}, mInit}; i != mIterations;
-                         i += vpIterationsFactor, m_ += mStep * vpIterationsFactor) {
-                        for (auto [j, k_] = std::tuple{vmcp::IntType{0}, kInit}; j != kIterations;
-                             j += vpIterationsFactor, k_ += kStep * vpIterationsFactor) {
+                    for (auto [j, k_] = std::tuple{vmcp::IntType{0}, kInit}; j != kIterations;
+                         j += vpIterationsFactor, k_ += kStep * vpIterationsFactor) {
+                        PotRad potRad{kInit};
+
+                        for (auto [i, m_] = std::tuple{vmcp::IntType{0}, mInit}; i != mIterations;
+                             i += vpIterationsFactor, m_ += mStep * vpIterationsFactor) {
                             vmcp::VarParam bestParam{k_ * m_.val / std::pow(vmcp::hbar, 2)};
                             vmcp::ParamBounds<1> const parBound{
                                 NiceBound(bestParam, minParamFactor, maxParamFactor, maxParDiff)};
                             vmcp::Energy const expectedEn{-k_ * k_ * m_.val / (2 * vmcp::hbar * vmcp::hbar)};
 
                             auto startOnePar = std::chrono::high_resolution_clock::now();
-                            vmcp::VMCResult const vmcr = vmcp::VMCEnergy<3, 1, 1>(
+                            vmcp::VMCResult const vmcr = vmcp::VMCEnergy<1, 1, 1>(
                                 wavefRad, parBound, laplRad, std::array{m_}, potRad, coordBound, numEnergies,
            rndGen); auto stopOnePar = std::chrono::high_resolution_clock::now(); auto durationOnePar =
            duration_cast<std::chrono::seconds>(stopOnePar - startOnePar); file_stream << "Radial problem, one
-           var.parameter, with mass" << m_.val
+           var.parameter, with mass " << m_.val
                                         << " (seconds): " << durationOnePar.count() << '\n';
 
                             std::string logMessage{"mass: " + std::to_string(m_.val) +
@@ -191,12 +147,12 @@ TEST_CASE("Testing the radial problem") {
                                               max(vmcr.stdDev * allowedStdDevs, stdDevTolerance),
                                           logMessage);
                         }
-
-                        auto stop = std::chrono::high_resolution_clock::now();
-                        auto duration = duration_cast<std::chrono::seconds>(stop - start);
-                        file_stream << "Radial problem, one var. parameter (seconds): " << duration.count() <<
-           '\n';
                     }
+
+                    auto stop = std::chrono::high_resolution_clock::now();
+                    auto duration = duration_cast<std::chrono::seconds>(stop - start);
+                    file_stream << "Radial problem, one var. parameter (seconds): " << duration.count() <<
+           '\n';
                 }*/
     }
 }
